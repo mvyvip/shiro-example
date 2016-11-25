a2cdb79afcc4cf0834e5235adedddef95b133b9f
Your Name <you@example.com>
2016-11-25 14:46:36 +0800

    e

---
+++ src/main/java/com/lt/shiro/ShiroUtil.java
@@ -0,0 +1,10 @@
+package com.lt.shiro;
+
+/**
+ * Created by lt on 2016/11/25 0025.
+ */
+public class ShiroUtil {
+    public static void main(String[] args) {
+        System.out.printf("xxx");
+    }
+}
